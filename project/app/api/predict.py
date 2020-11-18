29d16e23a536f48e6211499a6deb4b3f833a5bd2
skhabiri <shahnam.khabiri@gmail.com>
2020-11-18 00:25:21 -0800

    adds prediction route to API

--- project/app/api/predict.py
+++ project/app/api/predict.py
@@ -1,5 +1,6 @@
 import logging
 import random
+import pickle
 
 from fastapi import APIRouter
 import pandas as pd
@@ -9,47 +10,59 @@ log = logging.getLogger(__name__)
 router = APIRouter()
 
 
-class Item(BaseModel):
-    """Use this data model to parse the request body JSON."""
+class Item_query(BaseModel):
+    """Selected columns used in the model in JSON format"""
 
-    x1: float = Field(..., example=3.14)
-    x2: int = Field(..., example=-42)
-    x3: str = Field(..., example='banjo')
+    bridge_classification: str = Field(..., example='Standard')
+    bridge_opportunity_bridge_type: str = Field(..., example='Suspension Bridge')
+    bridge_opportunity_span_m: float = Field(..., example=85.0)
+    days_per_year_river_is_flooded: float = Field(..., example=121.0)
+    flag_for_rejection: str = Field(..., example='No')
+    height_differential_between_banks: float = Field(..., example=0.97)
 
     def to_df(self):
         """Convert pydantic object to pandas dataframe with 1 row."""
         return pd.DataFrame([dict(self)])
 
-    @validator('x1')
-    def x1_must_be_positive(cls, value):
-        """Validate that x1 is a positive number."""
-        assert value > 0, f'x1 == {value}, must be > 0'
-        return value
+    # @validator('input1')
+    # def title_must_be_a_string(cls, value):
+    #     """Validate that Title is a string."""
+    #     assert type(value) == str, f'Title == {value}, must be a string'
+    #     return value
+    #
+    # @validator('output1')
+    # def post_must_be_a_string(cls, value):
+    #     """Validate that post is a string."""
+    #     assert type(value) == str, f'Title == {value}, must be a string'
+    #     return value
 
 
-@router.post('/predict')
-async def predict(item: Item):
+@router.post('/prediction')
+async def predict(item: Item_query):
     """
-    Make random baseline predictions for classification problem 🔮
+    Returns Good_Site Prediction 0, 1
 
     ### Request Body
-    - `x1`: positive float
-    - `x2`: integer
-    - `x3`: string
+    ['bridge_opportunity_bridge_type',
+    'bridge_opportunity_span_m',
+    'days_per_year_river_is_flooded',
+    'bridge_classification',
+    'flag_for_rejection',
+    'height_differential_between_banks']
 
-    ### Response
-    - `prediction`: boolean, at random
-    - `predict_proba`: float between 0.5 and 1.0, 
-    representing the predicted class's probability
-
-    Replace the placeholder docstring and fake predictions with your own model.
     """
+    
+    query = item.to_df()
 
-    X_new = item.to_df()
-    log.info(X_new)
-    y_pred = random.choice([True, False])
-    y_pred_proba = random.random() / 2 + 0.5
-    return {
-        'prediction': y_pred,
-        'probability': y_pred_proba
-    }
+    def modelpredict(model, query):
+        """
+        query is dataframe for selected columns
+        """
+        return model.predict(query)[0], model.predict_proba(query)[0][int(model.predict(query)[0])]
+
+    
+    model = pickle.load(open("./app/api/gs_model",'rb'))
+
+    (y_pred, y_proba) = modelpredict(model, query)
+
+    return {'Good Site prediction': y_pred, 'Predicted Probability': y_proba}
